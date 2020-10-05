fcb54dbd16772d0ea19f3bb5385b9d3ae6d8c61c
lzz688 <lzz688@git.com>
2020-10-05 15:06:56 +0800

    Nacos分布式配置中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/lzz/springcloudalibaba/controller/OrderNacosController.java
@@ -0,0 +1,31 @@
+package com.lzz.springcloudalibaba.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+@Component
+public class OrderNacosController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //读取配置文件
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String PaymentInfo(@PathVariable("id") Long id){
+
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+}
